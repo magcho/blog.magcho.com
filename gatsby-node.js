a1bd9ca880bff0b311c786043d5e963ec3c6bece
magcho <mail@magcho.com>
2019-06-03 16:37:51 +0900

    [fix] pagenateの一ページあたりの記事数をふやした close #6

--- gatsby-node.js
+++ gatsby-node.js
@@ -62,7 +62,7 @@ exports.createPages = ({ graphql, actions }) => {
 
         // Create blog posts pages.
         const posts = result.data.allMarkdownRemark.edges
-        const postsPerPage = 4
+        const postsPerPage = 10
         const numPages = Math.ceil(posts.length / postsPerPage)
         Array.from({ length: numPages }).forEach((_, i) => {
           createPage({
