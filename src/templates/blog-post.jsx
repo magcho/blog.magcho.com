6b3a575e7d507d9146804d519143d3c4e23509e3
magcho <mail@magcho.com>
2021-03-14 00:29:49 +0900

    Update template, blog post

--- src/templates/blog-post.jsx
+++ src/templates/blog-post.jsx
@@ -1,7 +1,6 @@
-import React from 'react'
+import * as React from 'react'
 import Helmet from 'react-helmet'
 import { graphql } from 'gatsby'
-import get from 'lodash/get'
 
 import Layout from '../components/layout'
 import Penguin from '../components/penguin'
@@ -10,67 +9,49 @@ import Tags from '../components/tags'
 import PostTitle from '../components/posttitle'
 import Ogp from '../components/ogp'
 
-class BlogPostTemplate extends React.Component {
-  render() {
-    const post = this.props.data.markdownRemark
-    const siteTitle = get(this.props, 'data.site.siteMetadata.title')
-    const postDescription = post.excerpt
-    const { previous, next } = this.props.pageContext
-    const tagsList = get(this.props, 'data.allMarkdownRemark.group')
-    return (
-      <Layout
-        location={this.props.location}
-        siteTitle={siteTitle}
-        tagsList={tagsList}
-        previous={previous}
-        next={next}
-        parent={'blog-post'}
-      >
-        <Helmet
-          htmlAttributes={{ lang: 'ja' }}
-          meta={[
-            {
-              name: 'description',
-              content: postDescription,
-            },
-          ]}
-          title={`${post.frontmatter.title} | ${siteTitle}`}
-        />
-        <Ogp props={this.props} />
-        {/* <SnsOgp
-          pageUrl={this.props.location.pathname}
-          directLink={this.props.location.href}
-          image={`${this.props.origin}/twitter-icon.jpg`}
-          title={siteTitle}
-          description={postDescription}
-        /> */}
-        <article key={post.id}>
-          <div className={'content-header'}>
-            <div className={'title'}>
-              <PostTitle category={post.frontmatter.category}>
-                {post.frontmatter.title}
-              </PostTitle>
-            </div>
-            <div className={'date'}>
-              <Penguin
-                category={post.frontmatter.category}
-                date={post.frontmatter.date}
-              />
-            </div>
+const BlogPostTemplate = ({ data, location, pageContext }) => {
+  const post = data.markdownRemark
+  const siteTitle = data.site.siteMetadata.title
+  const postDescription = post.excerpt
+  const { previous, next } = pageContext
+  const tagsList = data.allMarkdownRemark.group
+
+  return (
+    <Layout
+      location={location}
+      siteTitle={siteTitle}
+      tagsList={tagsList}
+      previous={previous}
+      next={next}
+      parent={'blog-post'}
+    >
+      <Helmet
+        htmlAttributes={{ lang: 'ja' }}
+        meta={[
+          {
+            name: 'description',
+            content: postDescription,
+          },
+        ]}
+        title={`${post.frontmatter.title} | ${siteTitle}`}
+      />
+      <Ogp title={siteTitle} description={postDescription} />
+
+      <article key={post.id}>
+        <div className={'content-header'}>
+          <div className={'title'}>
+            <PostTitle category={post.frontmatter.category}>{post.frontmatter.title}</PostTitle>
           </div>
-          <Tags
-            list={post.frontmatter.tags || []}
-            category={post.frontmatter.category || []}
-          />
-          <div
-            className={'content-body'}
-            dangerouslySetInnerHTML={{ __html: post.html }}
-          />
-          {/* <Tags list={post.frontmatter.tags || [] } category={post.frontmatter.category || []}/> */}
-        </article>
-      </Layout>
-    )
-  }
+          <div className={'date'}>
+            <Penguin category={post.frontmatter.category} date={post.frontmatter.date} />
+          </div>
+        </div>
+        <Tags list={post.frontmatter.tags || []} category={post.frontmatter.category || []} />
+        <div className={'content-body'} dangerouslySetInnerHTML={{ __html: post.html }} />
+        <Tags list={post.frontmatter.tags || []} category={post.frontmatter.category || []} />
+      </article>
+    </Layout>
+  )
 }
 
 export default BlogPostTemplate
