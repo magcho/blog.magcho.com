0c317c46f271c4554282562ce8e64ab7cb9b1a1f
magcho <mail@magcho.com>
2021-03-13 23:37:28 +0900

    wip index

--- src/templates/index.jsx
+++ src/templates/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link, graphql } from 'gatsby'
-import get from 'lodash/get'
+
 import Helmet from 'react-helmet'
 
 import Layout from '../components/layout'
@@ -11,63 +11,63 @@ import PostTitle from '../components/posttitle'
 import ReadMore from '../components/readmore'
 import Ogp from '../components/ogp'
 
-class BlogIndex extends React.Component {
-  render() {
-    const siteMetadata = this.props.data.site.siteMetadata
-    const posts = this.props.data.allMarkdownRemark.edges
-    const tagsList = this.props.data.allMarkdownRemark.group
+const BlogIndex = ({ data, location, pageContext }) => {
+  const siteMetadata = data.site.siteMetadata
+  const posts = data.allMarkdownRemark.edges
+  const tagsList = data.allMarkdownRemark.group
 
-    // const index = this.props.pageContext.index
-    const currentPageNum = this.props.pageContext.currentPage
-    let previousUrl
-    if (currentPageNum >= 2) {
-      previousUrl = currentPageNum - 1 != 1 ? (currentPageNum - 1).toString() : '/'
-    } else {
-      previousUrl = ''
-    }
-    const lastPageFlag = this.props.pageContext.numPages == currentPageNum
-    const nextUrl = lastPageFlag ? '' : (currentPageNum + 1).toString()
-
-    return (
-      <Layout
-        location={this.props.location}
-        siteTitle={siteMetadata.title}
-        tagsList={tagsList}
-        previous={previousUrl}
-        next={nextUrl}
-        lastPageFlag={lastPageFlag}
-      >
-        <Helmet
-          htmlAttributes={{ lang: 'ja' }}
-          meta={[{ name: 'description', content: siteMetadata.description }]}
-          title={siteMetadata.title}
-        />
-        <Ogp props={this.props} />
-        {posts.map(({ node }) => {
-          return (
-            <article key={node.fields.slug}>
-              <div className={'content-header'}>
-                <div className={'title'}>
-                  <Link to={node.fields.slug}>
-                    <PostTitle category={node.frontmatter.category}>{node.frontmatter.title}</PostTitle>
-                  </Link>
-                </div>
-                <Penguin category={node.frontmatter.category} date={node.frontmatter.date} />
-              </div>
-              <div className={'content-body'}>
-                <p className={'excerpt'} dangerouslySetInnerHTML={{ __html: node.excerpt }} />
-              </div>
-              <ReadMore category={node.frontmatter.category} slug={node.fields.slug} />
-              <Tags list={node.frontmatter.tags || []} category={node.frontmatter.category || []} />
-            </article>
-          )
-        })}
-      </Layout>
-    )
+  // const index = this.props.pageContext.index
+  const currentPageNum = pageContext.currentPage
+  let previousUrl
+  if (currentPageNum >= 2) {
+    previousUrl = currentPageNum - 1 != 1 ? (currentPageNum - 1).toString() : '/'
+  } else {
+    previousUrl = ''
   }
+  const lastPageFlag = pageContext.numPages == currentPageNum
+  const nextUrl = lastPageFlag ? '' : (currentPageNum + 1).toString()
+
+  return (
+    <Layout
+      location={location}
+      siteTitle={siteMetadata.title}
+      tagsList={tagsList}
+      previous={previousUrl}
+      next={nextUrl}
+      lastPageFlag={lastPageFlag}
+    >
+      <Helmet
+        htmlAttributes={{ lang: 'ja' }}
+        meta={[{ name: 'description', content: siteMetadata.description }]}
+        title={siteMetadata.title}
+      />
+      {/* <Ogp props={this.props} /> */}
+      {posts.map(({ node }) => {
+        return (
+          <article key={node.fields.slug}>
+            <div className={'content-header'}>
+              <div className={'title'}>
+                <Link to={node.fields.slug}>
+                  <PostTitle category={node.frontmatter.category}>{node.frontmatter.title}</PostTitle>
+                </Link>
+              </div>
+              <Penguin category={node.frontmatter.category} date={node.frontmatter.date} />
+            </div>
+            <div className={'content-body'}>
+              <p className={'excerpt'} dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+            </div>
+            <ReadMore category={node.frontmatter.category} slug={node.fields.slug} />
+            <Tags list={node.frontmatter.tags || []} category={node.frontmatter.category || []} />
+          </article>
+        )
+      })}
+    </Layout>
+  )
 }
 
-export const blogListQuery = graphql`
+export default BlogIndex
+
+export const BlogListQuery = graphql`
   query blogListQuery($skip: Int!, $limit: Int!) {
     site {
       siteMetadata {
@@ -111,5 +111,3 @@ export const blogListQuery = graphql`
     }
   }
 `
-
-export default BlogIndex
