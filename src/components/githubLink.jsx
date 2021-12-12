9134b690f0b408a66503539aec84978235a86651
magcho <mail@magcho.com>
2021-12-12 18:19:31 +0900

    Fix CI failed

--- src/components/githubLink.jsx
+++ src/components/githubLink.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
 const GithubLink = ({ filePath }) => {
-  const relatedFilePath = filePath.match(/\/blog\.magcho\.com\/(.*)$/)[1]
-  const githubUrl = `https://github.com/magcho/blog.magcho.com/edit/master/${relatedFilePath}`
+  const relatedFilePath = filePath.match(/\/src\/pages\/.*/)
+  const githubUrl = `https://github.com/magcho/blog.magcho.com/edit/master${relatedFilePath}`
   return (
     <div className="github-link">
       <a href={githubUrl}>この記事の編集</a>
